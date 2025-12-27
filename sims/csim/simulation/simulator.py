90ce0e55590898bc83d2e2bd40e2cc8741220cd4
Colin Skinner <colinaskinner@gmail.com>
2025-12-27 11:04:36 -0800

    Basic Simulation Framework (#8)
    Full simulation, not individual timesteps yet
    
    * works for sat and orbit
    
    * Molniya and GEO
    
    * A little bit more testing the sim
    
    * Cleaned up simulation part and added a_meas and w_meas

--- sims/csim/simulation/simulator.py
+++ sims/csim/simulation/simulator.py
@@ -1,47 +1,75 @@
 import numpy as np
+from numpy.linalg import norm
+
 from ..entities import Spacecraft
-from ..math.integrators import rk4_func
+from ..math import rk4_func, quat_apply, unit
 from ..physics.rigid_body import rigid_body_derivative, RigidBodyParams
-
+from ..physics import grav_accel
+from ..world import MU_EARTH
+from ..physics.energy import calc_potential_energy, calc_kinetic_energy
 # TODO: def finish this
-class Simulator:
-    def __init__(self, t0: float, dt: float, n_steps: float, spacecraft: Spacecraft):
 
-        self.t_arr = np.linspace(t0, dt*n_steps, n_steps)
+# TODO: Different integrators for better ECI-ECEF OR interpolate every second or something
+
+class Simulator:
+    def __init__(self, state0: np.ndarray, t0: float, dt: float, n_steps: float, spacecraft: Spacecraft):
+
         self.t = t0
         self.dt = dt
         self.n_steps = n_steps
         self.sc = spacecraft
 
         self.X = np.zeros((n_steps+1, 13))
-        self.X[0] = self.sc.state
+        self.a_meas = np.zeros((n_steps+1, 3))
+        self.w_meas = np.zeros((n_steps+1, 3))
+        self.X[0] = state0
 
-    def step_one(self, step: int):
+        self.a_meas[0] = [0,0,0]
+        self.w_meas[0] = [0,0,0]
+
+    def step_one(self, state: np.ndarray):
         """Return false if simulation is done"""
-        
-        force = np.array([0, 0, self.sc.thrust(self.t)]) + np.array([0,0,-9.81*self.sc.mass])
 
+        # Get measured angular velocity
+        w = state[10:13]
+        q_B2I = unit(state[6:10]) # Turns body vector into inertial vector
+        # q_B2I = state[6:10] # 
+        w_body = quat_apply(q_B2I, w)
+
+
+        # Simulate next state
+        grav = grav_accel(state[:3])
+        accel = grav
+        a_meas = accel - grav
+
+        # Propagate 1 step
         sc = self.sc
-        params = RigidBodyParams(sc.mass, sc.I, force, np.zeros(3))
+        params = RigidBodyParams(sc.mass, sc.I, accel*sc.mass, np.zeros(3))
 
-        next_state = rk4_func(self.t, self.dt, self.sc.state, rigid_body_derivative, params)
-        self.sc.state = next_state
-        # print(next_state)
-        self.X[step+1] = next_state
+        next_state = rk4_func(self.t, self.dt, state, rigid_body_derivative, params)
+
+        next_state[6:10] = unit(next_state[6:10])
+        
+        return next_state, a_meas, w_body
 
     def simulate(self):
 
         self.final_step = self.n_steps
-        for step in range(1,self.n_steps):
-            
+        
+        for step in range(1,self.n_steps+1):
+
+            next_step, a_meas, w_meas = self.step_one(self.X[step-1])
             self.t += self.dt
-            self.step_one(step)
+
+            self.X[step] = next_step
+            self.a_meas[step] = a_meas
+            self.w_meas[step] = w_meas
 
             # Vz
-            if step > 10 and self.sc.state[2] <= 0:
-                # breakpoint()
-                self.final_step = step
-                break
+            # if step > 10 and self.X[step][2] <= 0:
+            #     # breakpoint()
+            #     self.final_step = step
+            #     break
 
 
         self.X = self.X[:self.final_step]
--- sims/test/simulation/test_simulator.py
+++ sims/test/simulation/test_simulator.py
@@ -6,21 +6,76 @@ import matplotlib.pyplot as plt
 
 sys.path.append(".")
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 from csim.simulation import Simulator
 from csim.entities import Spacecraft
+from csim.world import MU_EARTH, R_EARTH
+from csim.visualization import plot_orbit
 
-t0 = 0
-dt = 0.01
-n_steps = 10000
 
-state0 = np.array([0,0,0, 0,0,0, 0,0,0,0, 0,0,0])
+# TODO: actual tests?
+if __name__ == "__main__":
+    r = R_EARTH + 14500e3
 
-sc = Spacecraft(100, np.identity(3), state0)
-sim = Simulator(t0, dt, n_steps, sc)
+    v = np.sqrt(MU_EARTH/r)
+    print(r)
+    print(v)
 
-sim.simulate()
+    dt = 1
+    t0 = 0
+    n_steps = 100000
+    i = 98 * np.pi/180
+    state0 = np.array([
+        r*np.cos(i),0,r*np.sin(i), 
+        0,v,0, 
+        1,0,0,0, 
+        0.0001,0,0])
+    
+    m = 100
+    I = np.diag([2,2,2])
 
-print(sim.X.shape)
-plt.plot(sim.X[:,:3])
-plt.plot(sim.X[:,:6])
-plt.show()
+    sc = Spacecraft(m, None)
+    sim = Simulator(state0, t0, dt, n_steps, sc)
+
+    # sim.simulate()
+
+    # # n = 100
+    # # plt.plot(sim.X[:n,0], sim.X[:n,1])
+    # # plt.axis('equal')
+    # # plt.xlim()
+    # # plt.show()
+
+    # plot_orbit(sim.X)
+    
+    # plt.plot(sim.X[:,:3])
+    # plt.show()
+
+    # plt.plot(sim.X[:,3:6])
+    # plt.show()
+
+    # plt.plot(sim.X[:,6:10])
+    # plt.show()
+
+    # plt.plot(sim.X[:,10:13])
+    # plt.show()
+
+
+
+
+# t0 = 0
+# dt = 0.01
+# n_steps = 10000
+
+# state0 = np.array([0,0,0, 0,0,0, 0,0,0,0, 0,0,0])
+
+# sc = Spacecraft(100, np.identity(3), state0)
+# sim = Simulator(t0, dt, n_steps, sc)
+
+# sim.simulate()
+
+# print(sim.X.shape)
+# plt.plot(sim.X[:,:3])
+# plt.plot(sim.X[:,:6])
+# plt.show()
